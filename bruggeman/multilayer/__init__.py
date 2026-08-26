88debb0cd102b7d559625c7c256b93d967b9b9fb
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-08-26 11:18:37 +0200

    Start work on implementing multi-layer solutions

---
+++ bruggeman/multilayer/__init__.py
@@ -0,0 +1,11 @@
+"""C: Multi-layer systems (solutions 700-800)
+
+Submodules:
+
+- bruggeman.multilayer.flow1d: One dimensional flow in continuous multi-layer systems (solutions 710)
+- bruggeman.multilayer.radial: Two-dimensional radial flow in continuous multi-layer systems (solutions 720)
+"""
+
+# ruff: noqa F401
+from bruggeman.multilayer import flow1d, funcs
+from bruggeman.multilayer.flow1d import *
